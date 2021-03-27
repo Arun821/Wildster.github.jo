2e36c572b35698717a0699a387ed101d94376e7e
Arun281 <arunpkvky@gmail.com>
2021-03-27 11:45:54 +0530

    Auto-generated commit

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+ 
+  function openReg(){
+      document.getElementById("card").style.transform = "rotateX(180deg)";
+  }
+  function openLogin(){
+      document.getElementById("card").style.transform = "rotateX(0deg)";
+  }
